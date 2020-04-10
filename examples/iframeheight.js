156f4765ee22b279ac6b1cc554fb24df40e0c1dc
spurreiter <spurreiter@users.noreply.github.com>
2020-04-10 22:34:51 +0200

    chore(gh-pages): fix iframe height

---
+++ examples/iframeheight.js
@@ -0,0 +1,15 @@
+;(function () {
+  function docHeight () {
+    var body = document.body, html = document.documentElement
+    return Math.max(body.scrollHeight, body.offsetHeight,
+        html.clientHeight, html.scrollHeight, html.offsetHeight)
+  }
+
+  function handleMsg (e) {
+    const height = docHeight()
+    parent.postMessage(String(height), location.origin)
+  }
+
+  window.addEventListener('load', handleMsg, false)
+  new MutationObserver(handleMsg).observe(document.body, {subTree: true, childList: true})
+})()
